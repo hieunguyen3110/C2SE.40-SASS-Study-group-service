c73698e578d85a0ab9711bd21d0727791e8a712e
nguyenhieu31 <hieufaptv777@gmail.com>
2025-02-24 17:40:52 +0700

    config: set up application

---
+++ src/main/java/org/com/studygroupservice/StudyGroupServiceApplication.java
@@ -0,0 +1,13 @@
+package org.com.studygroupservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyGroupServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyGroupServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/com/studygroupservice/StudyGroupServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.com.studygroupservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyGroupServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
